bdf26fe639bd3647958c2c8a7b09093d889c3f5e
kinath <kinath.ru@gmail.com>
2019-08-03 12:52:10 +0530

    volatile keyword

--- src/main/java/edu/kinath/home/volatilekeyword/UserInterruptApp.java
+++ src/main/java/edu/kinath/home/volatilekeyword/UserInterruptApp.java
@@ -2,6 +2,13 @@ package edu.kinath.home.volatilekeyword;
 
 import java.util.Scanner;
 
+/**
+ * This demo is for explaining the usage of 'volatile' keyword.
+ *
+ * Different implementations of java might save the status of the thread variables in cache. This caching would prevent the thread from reading
+ * the correct value. 'volatile' variables are not cached. Therefore thread will always read the current value of the variable.
+ *
+ */
 public class UserInterruptApp
 {
     public static void main( String[] args )
@@ -19,7 +26,7 @@ public class UserInterruptApp
 
 class Processor extends Thread
 {
-    private boolean shutdown;
+    private volatile boolean shutdown;
 
     @Override
     public void run()
@@ -27,6 +34,15 @@ class Processor extends Thread
         while( !shutdown )
         {
             System.out.println("Hello ");
+
+            try
+            {
+                Thread.sleep( 100 );
+            }
+            catch( InterruptedException e )
+            {
+                e.printStackTrace();
+            }
         }
     }
 
